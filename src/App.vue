53a9b4a29d1a05ff03c127def30ca1d47b865e4e
Andrey Gavrilov <boombardir20@gmail.com>
2022-01-26 15:28:14 +0500

    omg close

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,32 @@
 <template>
-  <div id="app">
+  <div
+    id="app"
+    :class="
+      typeof weather.main != 'undefined' && weather.main.temp > 16 ? 'warm' : ''
+    "
+  >
     <main>
       <div class="search-box">
         <input
-          v-model="query"
-          @keypress="fetchWeather"
           type="text"
           class="search-bar"
           placeholder="Search..."
+          v-model="query"
+          @keypress="fetchWeather"
         />
       </div>
-      <div class="weather-wrap">
+
+      <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
         <div class="location-box">
-          <div class="location">LYSVA PERMSKY CRY</div>
-          <div class="date">Monday 20 janury 2022</div>
+          <div class="location">
+            {{ weather.name }}, {{ weather.sys.country }}
+          </div>
+          <div class="date">{{ dateBuilder() }}</div>
         </div>
+
         <div class="weather-box">
-          <div class="temp">9°C</div>
-          <div class="weather">Rain</div>
+          <div class="temp">{{ Math.round(weather.main.temp) }}°c</div>
+          <div class="weather">{{ weather.weather[0].main }}</div>
         </div>
       </div>
     </main>
@@ -26,16 +35,15 @@
 
 <script>
 export default {
-  name: "App",
+  name: "app",
   data() {
     return {
       api_key: "0f0f5f2a06f7cbf5eb2c1fd66a268ef2",
-      url_base: "http://api.openweathermap.org/data/2.5/",
+      url_base: "https://api.openweathermap.org/data/2.5/",
       query: "",
       weather: {},
     };
   },
-  components: {},
   methods: {
     fetchWeather(e) {
       if (e.key == "Enter") {
@@ -47,6 +55,40 @@ export default {
           })
           .then(this.setResults);
       }
+    },
+    setResults(results) {
+      this.weather = results;
+    },
+    dateBuilder() {
+      let d = new Date();
+      let months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+      ];
+      let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+      ];
+      let day = days[d.getDay()];
+      let date = d.getDate();
+      let month = months[d.getMonth()];
+      let year = d.getFullYear();
+      return `${day} ${date} ${month} ${year}`;
     },
   },
 };
@@ -67,6 +109,9 @@ body {
   background-position: bottom;
   transition: 0.4s;
 }
+#app.warm {
+  background-image: url("./assets/warm-bg.jpg");
+}
 main {
   min-height: 100vh;
   padding: 25px;
@@ -84,9 +129,9 @@ main {
   display: block;
   width: 100%;
   padding: 15px;
+
   color: #313131;
   font-size: 20px;
-
   appearance: none;
   border: none;
   outline: none;
@@ -97,20 +142,19 @@ main {
   transition: 0.4s;
 }
 .search-box .search-bar:focus {
-  background-color: rgba(255, 255, 255, 0.75);
   box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.75);
   border-radius: 16px 0px 16px 0px;
 }
-
 .location-box .location {
-  color: white;
+  color: #fff;
   font-size: 32px;
   font-weight: 500;
   text-align: center;
   text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
 }
 .location-box .date {
-  color: white;
+  color: #fff;
   font-size: 20px;
   font-weight: 300;
   font-style: italic;
@@ -122,18 +166,17 @@ main {
 .weather-box .temp {
   display: inline-block;
   padding: 10px 25px;
-  color: white;
+  color: #fff;
   font-size: 102px;
   font-weight: 900;
-
   text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
-  background-color: rgba(255, 255, 255, 0.5);
+  background-color: rgba(255, 255, 255, 0.25);
   border-radius: 16px;
   margin: 30px 0px;
   box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
 }
 .weather-box .weather {
-  color: white;
+  color: #fff;
   font-size: 48px;
   font-weight: 700;
   font-style: italic;
